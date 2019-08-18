1c55dfb93b5a1bbd38c2f02c5a918e73bc0dde49
Fahd258 <54243960+Fahd258@users.noreply.github.com>
2019-08-18 20:12:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ const prefix = '!!'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`!!help| !!invite`)
+client.user.setGame(`المطور فهد بن محمد `)
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
